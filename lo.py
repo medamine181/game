89e6dc757094ed5fb6bfbd6ad1519888e30d6c29
vipar_da7i7 <117691465+medamine181@users.noreply.github.com>
2025-05-11 14:28:43 +0100

    Update and rename README.md to lo.py

---
+++ lo.py
@@ -0,0 +1,14 @@
+ch = input(">> ")
+if ch <= "8" and ch >= "8":
+    if ch[0] == "2":
+        print("ooredo")
+    elif ch[0] == "5":
+        print("oronge")
+    elif ch[0] == "7":
+        print("sousite")
+    elif ch[0] == "9":
+        print("telecom")
+    else :
+        print("le numero n'est pas valide")
+else:
+    print("le numero n'est pas valide")
